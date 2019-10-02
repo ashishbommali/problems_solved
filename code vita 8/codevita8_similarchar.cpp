bcc455bb5e2b56a784ca9ef1ac67a10cb7675bb0
ashishbommali <39550160+ashishbommali@users.noreply.github.com>
2019-10-02 18:41:26 +0530

    Add files via upload

---
+++ code vita 8/codevita8_similarchar.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long int n;
+    scanf("%d",&n);
+    long int a[n];
+    for(long int i=0;i<n;i++)
+	scanf("%d",&a[i]);
+    
+     
+    return 0;
+}
